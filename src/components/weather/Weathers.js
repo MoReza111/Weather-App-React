689ee4029636e657f1d35e29ea48467381e8b0f9
MoReza <mz.abdollahi223@gmail.com>
2020-11-01 09:46:03 +0330

    intialize the project

---
+++ src/components/weather/Weathers.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CloudIcon from '@material-ui/icons/Cloud'
+import WbSunnyIcon from '@material-ui/icons/WbSunny'
+
+const Weathers = () => {
+    return (
+        <div className="container">
+            <div className="card">
+
+            </div>
+        </div>
+    )
+}
+
+export default Weathers
